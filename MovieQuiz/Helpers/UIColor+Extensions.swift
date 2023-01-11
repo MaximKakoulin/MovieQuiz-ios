27e25171e17b91e6fc0a130ec489511a40fa79f9
Maxim <maximm1213@gmail.com>
2023-01-11 16:32:04 +0700

    create a logic code in MovieQuizController and made up the layout in StoryBoard. Sprint_04

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,4 +1,13 @@
 import Foundation
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var ypBlack: UIColor { UIColor(named: "ypblack")! }
+    static var ypWhite: UIColor { UIColor(named: "ypWhite")! }
+    static var ypGreen: UIColor { UIColor(named: "ypGreen")! }
+    static var ypRed: UIColor { UIColor(named: "ypRed")! }
+    static var ypDarkGrey: UIColor { UIColor(named: "ypDarkGrey")! }
+    static var ypGrey: UIColor { UIColor(named: "ypGrey")! }
+}
+
+
