d070a1a3a851554214322c45d8275d4e879ef6a3
Maxim <maximm1213@gmail.com>
2023-02-04 21:50:40 +0700

    Added new file with a class AlertPresenter

---
+++ MovieQuiz/Models/AlertModel.swift
@@ -0,0 +1,10 @@
+import Foundation
+import UIKit
+
+// для создания алерта в конце квиза
+struct AlertModel {
+    let title: String
+    let message: String
+    let buttonText: String
+    let completion: ((UIAlertAction) -> Void)?
+}
